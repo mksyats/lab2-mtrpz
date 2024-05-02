7450f1cd0f85e19521a31f688da3ea82f8b33fa9
Maksym Yatsenko <saddas1rambler@gmail.com>
2024-05-02 20:39:34 +0300

    small changes in the way to ANSI conversion

--- test/convertMdToAnsi.test.js
+++ test/convertMdToAnsi.test.js
@@ -3,7 +3,7 @@ const convertMarkdownToAnsi = require('../src/convertMdToAnsi');
 describe('convertMarkdownToAnsi', () => {
   test('valid md to ANSI', () => {
     const markdown = '**Жовта сова** _на_ `старому дереві` відправилася на подорож\n```\nПід час **своєї_мандрівки**\n```\nвона зустріла _маленького_ їжачка.';
-    const expected = '\x1b[1mЖовта сова\x1b[0m \x1b[3mна\x1b[0m \x1b[7mстарому дереві\x1b[0m відправилася на подорож\n\x1b[7m\nПід час **своєї_мандрівки**\n\x1b[0m\nвона зустріла \x1b[3mмаленького\x1b[0m їжачка.';
+    const expected = '\x1b[1mЖовта сова\x1b[0m \x1b[3mна\x1b[0m \x1b[7mстарому дереві\x1b[0m відправилася на подорож\x1b[7m\nПід час **своєї_мандрівки**\n\x1b[0mвона зустріла \x1b[3mмаленького\x1b[0m їжачка.';
     expect(convertMarkdownToAnsi(markdown)).toEqual(expected);
   });
   test('invalid md', () => {
